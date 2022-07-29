404a770472cb5e7f82f087dca2d03bd5b8c0fdf6
Robel-Ak <Robelamde21@gmail.com>
2022-07-29 14:20:23 +0300

    admin role set

--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -14,7 +14,7 @@ export class AuthGuard implements CanActivate {
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       // let isLoggedIn = this.authenticationService.isAuthenticated();
       let principle = this.authenticationService.getAuthenticatedPrinciple();
-    if (principle && principle['isLoggedIn'] == true && principle['role'] == 'user'){
+    if (principle && principle['isLoggedIn'] == true && (principle['role'] == 'user' || principle['role'] == 'Admin' )){
       return true;
     } else {
       window.alert("Failed to signin and try again!!")
